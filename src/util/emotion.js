8a8867e96a1ba1f8a494bb83f49a7c0f9bc97d42
IMJOne <mompapa7@gmail.com>
2023-01-29 22:36:13 +0900

    Separate emotion list array

---
+++ src/util/emotion.js
@@ -0,0 +1,27 @@
+export const emotionList = [
+  {
+    emotion_score: 1,
+    emotion_image: `../img/emotion1.png`,
+    emotion_description: '완전 좋음',
+  },
+  {
+    emotion_score: 2,
+    emotion_image: `../img/emotion2.png`,
+    emotion_description: '좋음',
+  },
+  {
+    emotion_score: 3,
+    emotion_image: `../img/emotion3.png`,
+    emotion_description: '그럭 저럭',
+  },
+  {
+    emotion_score: 4,
+    emotion_image: `../img/emotion4.png`,
+    emotion_description: '나쁨',
+  },
+  {
+    emotion_score: 5,
+    emotion_image: `../img/emotion5.png`,
+    emotion_description: '끔찍함',
+  },
+];
